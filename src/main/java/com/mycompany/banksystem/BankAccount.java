1ebb944f7a048f571a2c0fa083c00d0863904721
Ruggery <ruggeryalves@hotmail.com>
2025-05-27 22:15:08 +0100

    Initializing a new project to use all I have learned in first year of college.

---
+++ src/main/java/com/mycompany/banksystem/BankAccount.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banksystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ruggery
+ */
+public class BankAccount {
+    private static int nextAccountNumber = 1;
+    private static ArrayList<BankAccount> userAccount = new ArrayList<>();
+    
+    private int AccountNumber;
+    private String AccountHolderName;
+    private double balance;
+
+    public BankAccount(String AccountHolderName) {
+        this.AccountHolderName = AccountHolderName;
+        this.balance = 0.0;
+        //auto-generator number
+        this.AccountNumber = nextAccountNumber++;
+        userAccount.add(this);
+    }
+
+    public static ArrayList<BankAccount> getUserAccount() {
+        return userAccount;
+    }
+
+    public static void setUserAccount(ArrayList<BankAccount> userAccount) {
+        BankAccount.userAccount = userAccount;
+    }
+    
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return AccountHolderName;
+    }
+
+    public void setAccountHolderName(String AccountHolderName) {
+        this.AccountHolderName = AccountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public String accountData(){
+        return "Account Number: " + AccountNumber + " Account Name: " + AccountHolderName + " Balance: " + balance;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/banksystem/BankSystem.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.banksystem;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ruggery
+ */
+public class BankSystem {
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //This will be the Scope of the program, a menu where everything will works on it. 
+        boolean running = true;
+
+        while (running) {
+            System.out.println("===== Bank Menu =====");
+            System.out.println("[1] - Create an account");
+            System.out.println("[2] - View All Accounts");
+            System.out.println("[3] - Deposit");
+            System.out.println("[4] - Withdraw");
+            System.out.println("[5] - Transfer");
+            System.out.println("[6] - Delete account");
+            System.out.println("[0] - Exit");
+
+            System.out.printf("Enter your option: ");
+            String option = scanner.nextLine();
+
+            switch (option) {
+                case "0":
+                    running = false;
+                    break;
+                case "1":
+                    System.out.printf("Enter your name: ");
+                    String name = scanner.nextLine();
+                    BankAccount account = new BankAccount(name);    
+                    System.out.println("Welcome, " + account.getAccountHolderName());
+                    account.accountData(); //need to delete it later
+                    break;
+                case "2":
+                    for(BankAccount bankAccount : BankAccount.getUserAccount()){
+                        System.out.println(bankAccount.accountData());
+                    }
+                    
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+            }
+
+        }
+    }
+}
